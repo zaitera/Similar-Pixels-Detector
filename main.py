95ca6a22a05f342e414f99875e4780a10bd2685c
Abdullah Zaiter <abdullah.zaiter@hotmail.com>
2019-03-28 20:45:12 -0300

    Adding final version of requisite 3 (applied and tested with videos)

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-import numpy as np
 import cv2
+import numpy as np
 from sys import exit
 from os import path
 
-CEND = '\33[0m'
-CBOLD = '\33[1m'
-CRED = '\33[31m'
-CGREEN = '\33[32m'
-CBLUE = '\33[34m'
+CEND    = '\33[0m'
+CBOLD   = '\33[1m'
+CRED    = '\33[31m'
+CGREEN  = '\33[32m'
+CBLUE   = '\33[34m'
 NEG = '\033[5;37;40m'
 NEGEND = '\033[0;37;40m'
 
@@ -17,31 +17,41 @@ def insertRedByBinary(binary_img, image, read_mode):
     b, g, r = cv2.split(image)
     binary_img = binary_img.astype(np.uint8)
     cv2.bitwise_or(r, binary_img, r)
-    cv2.bitwise_not(binary_img, binary_img)
+    cv2.bitwise_not(binary_img,binary_img)
     cv2.bitwise_and(b, binary_img, b)
     cv2.bitwise_and(g, binary_img, g)
-    return cv2.merge((b, g, r))
+    return cv2.merge((b,g,r))
 
-def distanceMatCalculator(pixel, image, read_mode):
+def distanceMatCalculator(pixel,image, read_mode):
     if(read_mode == cv2.IMREAD_ANYCOLOR):
-        img_aux = np.zeros((img.shape[0], img.shape[1], img.shape[2]))
-        img_aux[:] = pixel  # solid color image made of the clicked color
-        img_aux = img_aux - img
+        img_aux = np.zeros((img.shape[0],img.shape[1],img.shape[2]))
+        img_aux[:] = pixel #solid color image made of the clicked color
+        img_aux = img_aux - img  
         b, g, r = cv2.split(img_aux)
-        dist = np.zeros((img.shape[0], img.shape[1]))
-        dist = (b**2 + g**2 + r**2)  # euclidian distance
-        # threshold for the minimum distance required
-        cv2.threshold(dist, (13**2)-1, 255, cv2.THRESH_BINARY_INV, dist)
+        dist = np.zeros((img.shape[0],img.shape[1]))
+        dist = (b**2 + g**2 + r**2) #euclidian distance
+        cv2.threshold(dist,(13**2)-1,255,cv2.THRESH_BINARY_INV,dist) #threshold for the minimum distance required
         return dist
-    elif(read_mode == cv2.IMREAD_GRAYSCALE):
-        dist = np.zeros((img.shape[0], img.shape[1]))
+    elif(read_mode == cv2.IMREAD_GRAYSCALE):             
+        dist = np.zeros((img.shape[0],img.shape[1]))
         dist[:] = pixel
-        dist = abs(dist - img)
-        # threshold for the minimum distance required
-        cv2.threshold(dist, (12), 255, cv2.THRESH_BINARY_INV, dist)
+        dist = abs(dist - img)        
+        cv2.threshold(dist,(12),255,cv2.THRESH_BINARY_INV,dist) #threshold for the minimum distance required
         return dist
 
-def isImageOnGrayScale(image):
+def mouseCallBack(event,x,y,flags,param):
+    if event == cv2.EVENT_LBUTTONDOWN:    
+        global clicked_flag
+        global current_pixel_reference
+        clicked_flag = True
+        current_pixel_reference = img[y][x]
+        if(read_mode == cv2.IMREAD_ANYCOLOR):
+            print(CBOLD+"row: ",str(x), " column: ", str(y), CBLUE+" B: ",str(img[y][x][0]),CGREEN+" G: ",str(img[y][x][1]),CRED+"R: ",str(img[y][x][2])+CEND)
+        else:    
+            print(CBOLD+"row: ",str(x), " column: ", str(y), NEG+"Gray: ",str(img[y][x])+CEND)
+        pass
+
+def isImageInGrayScale(image):
     b, g, r = cv2.split(image)
     aux1 = b - g
     aux2 = g - r
@@ -52,38 +62,57 @@ def isImageOnGrayScale(image):
     else:
         return False
 
-def mouseCallBack(event, x, y, flags, param):
-    if event == cv2.EVENT_LBUTTONDOWN:
-        global clicked_flag
-        clicked_flag = True
-        dist = distanceMatCalculator(img[y][x], img, read_mode)
-        img_aux = insertRedByBinary(dist,img,read_mode)
-        if(read_mode == cv2.IMREAD_ANYCOLOR):
-            print(CBOLD+"row: ", str(x), " column: ", str(y), CBLUE+" B: ",
-                  str(img[y][x][0]), CGREEN+" G: ", str(img[y][x][1]), CRED+"R: ", str(img[y][x][2])+CEND)
-        else:
-            print(CBOLD+"row: ", str(x), " column: ",
-                  str(y), NEG+"Gray: ", str(img[y][x])+CEND)
-        cv2.namedWindow('Painted')
-        cv2.imshow('Painted', img_aux)
+def init():
+    file_type = input("For Video click 1 and for image click 2 ")
+    print(file_type)
+    if(not(file_type == "1" or file_type == "2")):
+        exit("Invalid option")  
+    file_name = input("What is the file's name: ")
+    print(file_name)
+    if(not(path.isfile("./"+file_name))):
+        exit("File doesn't exist or isn't in the local directory")
+    return file_type, file_name
 
 if __name__ == "__main__":
-    image_name = input("What is the image's name: ")
-    print(image_name)
-    if(not(path.isfile("./"+image_name))):
-        exit("File doesn't exist or isn't in the local directory")
-    img = cv2.imread(image_name, cv2.IMREAD_ANYCOLOR)
-    if(isImageOnGrayScale(img)):
-        print("Grayscale detected!")
-        read_mode = cv2.IMREAD_GRAYSCALE
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    file_type, file_name = init()
+    cap = cv2.VideoCapture(file_name)    
+    flag, img = cap.read()
+    if flag:
+        if(isImageInGrayScale(img)):
+            print("Grayscale detected!")
+            read_mode = cv2.IMREAD_GRAYSCALE
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        else:
+            print("RGB")
+            read_mode = cv2.IMREAD_ANYCOLOR
     else:
-        print("RGB")
-        read_mode = cv2.IMREAD_ANYCOLOR
+        exit("Error while reading the video, try again.")
     cv2.namedWindow('Original')
-    cv2.setMouseCallback('Original', mouseCallBack)
-    cv2.imshow('Original', img)
-    while(1):
-        if cv2.waitKey(20) == 27:
+    cv2.setMouseCallback('Original',mouseCallBack)
+    clicked_flag = False
+    while True:
+        flag, img = cap.read()
+        if flag:
+            # The frame is ready and already captured
+            if(read_mode == cv2.IMREAD_GRAYSCALE):
+                img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            cv2.imshow('Original', img)           
+            pos_frame = cap.get(cv2.CAP_PROP_POS_FRAMES)
+            if(clicked_flag):
+                distance = distanceMatCalculator(current_pixel_reference, img, read_mode)
+                result = insertRedByBinary(distance, img, read_mode)
+                cv2.namedWindow('Painted')
+                cv2.imshow('Painted',result)
+            #print (str(pos_frame)+" frames ", clicked_flag)
+        else:
+            # The next frame is not ready, so we try to read it again
+            cap.set(cv2.CAP_PROP_POS_FRAMES, pos_frame-1)
+            print ("frame is not ready")
+            # It is better 1to wait for a while for the next frame to be ready
+            cv2.waitKey(10)
+        if cv2.waitKey(25) == 27:
             break
-    cv2.destroyAllWindows()
+        if cap.get(cv2.CAP_PROP_POS_FRAMES) == cap.get(cv2.CAP_PROP_FRAME_COUNT):
+            # If the number of captured frames is equal to the total number of frames,
+            # we stop
+            break
